faa0fd48e62459d2b9c2f9b52bab9c4a9db0cc11
Nigel <nigel.james@squarecloud.com.au>
2015-03-22 16:21:24 +1100

    added apihelper for CCB

---
+++ app/Console/Commands/CicCommand.php
@@ -0,0 +1,141 @@
+<?php namespace sc\cic\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use GuzzleHttp\Event\BeforeEvent;
+use sc\cic\ApiHelpers\CcbApi;
+use Vinkla\Hashids;
+
+class CicCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'CicCommand';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Base command for Churchic';
+
+  protected $client;
+
+  protected $startTime;
+  protected $finishTime;
+
+  protected $ccbApi;
+  protected $hashids;
+  protected $mailchimp;
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+    $this->start();
+    $this->client = $this->argument('client');
+    $this->setApis();
+	}
+  protected function setApis(){
+
+    $this->hashids = new Hashids($this->client); // use clientname as seed
+
+    $clientConnection = \ClientConnection::where('client_id', '=', $this->client)
+                                          ->where('source_name', '=', 'CCB')->first();
+
+    $this->ccbApi = new CcbApi($clientConnection->client_id, $clientConnection->username, $clientConnection->password);
+
+    $clientConnection = \ClientConnection::where('client_id', '=', $this->client)
+                                          ->where('source_name', '=', 'Mailchimp')->first();
+
+    $this->mailchimp =  new \Mailchimp($clientConnection->apikey);
+
+  }
+
+  /**
+   * Tidy up after the fire has finished
+   *
+   */
+  public function tidyUp()
+  {
+    $this->finish();
+    $runTime = $this->runTime();
+    \Log::info("$this->name for $this->client ran in $runTime Seconds");
+  }
+
+  /**
+ 	 * Get the console command arguments.
+ 	 *
+ 	 * @return array
+ 	 */
+ 	protected function getArguments()
+ 	{
+ 		return array(
+ 			array('client', InputArgument::REQUIRED, 'Client id to run this commmand as'),
+ 		);
+ 	}
+
+ 	/**
+ 	 * Get the console command options.
+ 	 *
+ 	 * @return array
+ 	 */
+ 	protected function getOptions()
+ 	{
+ 		return array(
+// 			array('example_parent', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+ 		);
+ 	}
+
+
+  protected function start()
+  {
+    $this->startTime = microtime(true);
+  }
+
+  protected function finish()
+  {
+    $this->finishTime = microtime(true);
+  }
+
+  protected function runTime()
+  {
+    return $this->finishTime - $this->startTime;
+  }
+
+  protected function parseXml($input){
+
+    libxml_use_internal_errors(true);
+
+    $sxe = simplexml_load_string( $input->getBody() );
+
+    if ($sxe === false) {
+      $this->error("Failed loading XML");
+      \Log::error( "Failed loading XML in $this->name ");
+        foreach(libxml_get_errors() as $error) {
+          \Log::error($error->message);
+        }
+      return 1;
+    }
+
+    return $sxe;
+
+  }
+}
