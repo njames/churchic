e38c284186251f1b11dc634d3f262580a568506d
Nigel <nigel.james@squarecloud.com.au>
2015-09-24 16:27:48 +1000

    update of admin-lte

--- resources/views/admin/upload/index.blade.php
+++ resources/views/admin/upload/index.blade.php
@@ -1,23 +1,44 @@
-<html>
+@extends('app')
+
+@section('htmlheader_title')
+    Home
+@endsection
+
+@section('htmlheader_scripts')
+    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+@endsection
+
+
+@section('main-content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Home</div>
+
+				<div class="panel-body">
+
+
+                    <div class="dz-preview dz-file-preview">
+                      <div class="dz-details">
+                        <div class="dz-filename"><span data-dz-name></span></div>
+                        <div class="dz-size" data-dz-size></div>
+                        <img data-dz-thumbnail />
+                      </div>
+                      <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+                      <div class="dz-success-mark"><span>✔</span></div>
+                      <div class="dz-error-mark"><span>✘</span></div>
+                      <div class="dz-error-message"><span data-dz-errormessage></span></div>
+                    </div>
 
 
 
-<head>
-@{{ HTML::style('css/dropzone.css') }}
-@{{ HTML::script('js/dropzone.js') }}
 
-</head>
-<body>
-<div class="dz-preview dz-file-preview">
-  <div class="dz-details">
-    <div class="dz-filename"><span data-dz-name></span></div>
-    <div class="dz-size" data-dz-size></div>
-    <img data-dz-thumbnail />
-  </div>
-  <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
-  <div class="dz-success-mark"><span>✔</span></div>
-  <div class="dz-error-mark"><span>✘</span></div>
-  <div class="dz-error-message"><span data-dz-errormessage></span></div>
+
+
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-</body>
-</html>
+@endsection
--- resources/views/partials/htmlheader.blade.php
+++ resources/views/partials/htmlheader.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <title> AdminLTE 2 with Laravel - @yield('htmlheader_title', 'Your title here') </title>
+    <title> ChurchIC - @yield('htmlheader_title', 'Your title here') </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
     <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
@@ -24,4 +24,5 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    @yield('htmlheader_scripts')
 </head>
