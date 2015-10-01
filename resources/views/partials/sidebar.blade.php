e935b44a373e77015a8e4e6c139a018764c1d5c6
Nigel <nigel.james@squarecloud.com.au>
2015-10-02 06:54:34 +1000

    update to sidebar links

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -33,8 +33,9 @@
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>Home</span></a></li>
             <li><a href="{{ url('groups') }}"><i class='fa fa-link'></i> <span>Groups</span></a></li>
+            <li><a href="{{ url('upload') }}"><i class='fa fa-link'></i> <span>Upload</span></a></li>
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="#"><i class='fa fa-link'></i> <span>Multi-level</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
                     <li><a href="#">Link in level 2</a></li>
                     <li><a href="#">Link in level 2</a></li>
