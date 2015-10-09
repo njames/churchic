67096686a8c7e59501dcda1df7e2debfe7edc778
Nigel <nigel.james@squarecloud.com.au>
2015-10-09 17:06:49 +1100

    adding migration for photo event participants

---
+++ database/migrations/2015_10_08_100523_create_photo_event_participants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoEventParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_event_participants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('client_id', 30)->index();
+            $table->integer('photo_event_id')->unsigned();
+            $table->string('name', 60);
+            $table->string('email', 120);
+            $table->integer('assigned_number')->unsigned();
+            $table->string('path_photo_large',512);
+            $table->string('path_photo_small',512);
+            $table->timestamps();
+
+            $table->foreign('photo_event_id')->references('id')->on('photo_events')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photo_event_participants');
+    }
+}
